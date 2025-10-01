6f09b28e19290a56fc0749a818a9728cfebacc50
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-10-01 09:15:38 +0700

    Latihan

---
+++ Latihan03.java
@@ -0,0 +1,12 @@
+public class Latihan03 {
+
+    public static void main(String[] args) {
+            System.out.println("Halo, nama saya adalah Akmal!");
+
+            System.out.println("Belajar java itu mudah");
+            System.out.println("makin sering mencoba, semakin jago");
+    
+    }
+}
+    
+
