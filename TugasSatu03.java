b176b3125c7b7ee308828244a41d0e8953b82167
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-10-01 09:17:08 +0700

    Tugas 1

---
+++ TugasSatu03.java
@@ -0,0 +1,18 @@
+public class TugasSatu03 {
+
+    public static void main(String[] args) {
+        
+        System.out.printf("%-12s, %-20s, %-25s\n", "NIM", "NAMA", "PRODI");
+
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060093","M. MUFLIH R. F.","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060146", "AKHDAN NAJAM MA'ARU","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060174", "GILANG PAMUNGKAS J.","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060039", "AKMALIA RIMA HATI","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060136", "ALVIDO ARIESTA Z. W.","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060147", "ARIFATUL AFIDAH","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060066", "AULIA ZAHWA","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060098", "DEWI MUJI RAHAYU","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060138", "EVAN FERNANDA A.","Sistem Informasi Bisnis");
+        System.out.printf("%-5s, %-20s, %-25s\n", "254107060020", "FARA FITRIA N.","Sistem Informasi Bisnis");
+    }
+}
